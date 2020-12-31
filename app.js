efcfa644d171bee2f3d9eb233a3b412376fae8e2
maaarvin-was-here <37755670+maaarvin-was-here@users.noreply.github.com>
2020-12-31 03:46:33 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var titleAdjective = document.getElementById("titleChangingDescriptor");
+
+var adjectives = [
+    "Cool",
+    "Awesome",
+    "Perfection"
+];
+
+
+changeWord(0);
+
+    
+
+    
+
+
+
+function changeWord(x) {
+    if (x < adjectives.length){
+        var string = adjectives[x];
+    
+        setTimeout(() => {
+            titleAdjective.innerHTML = string;
+            changeWord(x + 1);
+        }, 1000);
+    } else {
+        changeWord(0);
+    }
+}
