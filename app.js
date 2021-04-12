4417e4ec9b0d68f952182fab09c77ec5b69abe22
maaarvin-was-here <37755670+maaarvin-was-here@users.noreply.github.com>
2021-04-11 23:27:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var titleAdjective = document.getElementById("titleChangingDescriptor");
+var titlePrefix = document.getElementById("titleChangingPrefix");
 
 var adjectives = [
-    "Cool",
-    "Awesome",
-    "Perfection"
+    "Engineer",
+    "Designer",
+    "Creator    "
+];
+
+var prefixes = [
+    "an&nbsp&nbsp;",
+    "a&nbsp;&nbsp;",
+    "a&nbsp;&nbsp;"
 ];
 
 
@@ -11,19 +18,27 @@ changeWord(0);
 
     
 
-    
-
-
-
 function changeWord(x) {
     if (x < adjectives.length){
+        var prefix = prefixes[x];
         var string = adjectives[x];
     
         setTimeout(() => {
             titleAdjective.innerHTML = string;
+            titlePrefix.innerHTML = prefix;
             changeWord(x + 1);
-        }, 1000);
+        }, 2000);
     } else {
         changeWord(0);
     }
 }
+
+
+/*var textBar = document.getElementById("titleExtra");
+
+window.onmousewheel = function () {
+    if (textBar.style.width < 100) {
+        textBar.style.width += 2;
+    }
+    console.log("scrolled");
+} */
