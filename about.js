90e79e210dd270f266ac9e523d8e7bd8aabd426c
maaarvin-was-here <37755670+maaarvin-was-here@users.noreply.github.com>
2021-04-13 12:52:00 -0700

    Update about.js

--- about.js
+++ about.js
@@ -4,12 +4,16 @@ var menu = document.getElementById("menu");
 
 var menuToggle = false;
 
+function reset(){
+    menu.style.zIndex = "-1";
+}
+
 function showMenu(){
     console.log(menu.style.zIndex);
     if(menuToggle){
         menuToggle = false;
         menu.style.zIndex = "-1";
-        menu.style.opacity = "1";
+        //menu.style.opacity = "1";
     } else {
         menu.style.opacity = "0.8"
         menuToggle = true;
